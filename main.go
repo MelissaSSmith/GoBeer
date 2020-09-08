0e2988d2f0fed8b579d2c4b78a16530278329119
Melissa Smith <melissasmith358@gmail.com>
2020-09-07 18:08:53 -0700

    correct requests and fix initialization of the router

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	router := NewRouter()
+	router := mux.NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
